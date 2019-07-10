8e7737963510f64bfcefeb736e8dc48513c72dee
Parradan <parrita.dan@hotmail.com>
2019-07-10 20:29:10 -0300

    Agregue librerias de DHT y un pedazo de codigo en el que funciona bien el DHT21

--- Green_House/NodeMcu/NodeMcu.ino
+++ Green_House/NodeMcu/NodeMcu.ino
@@ -1,22 +1,43 @@
+#include "DHT.h"
+#define DHTPIN 4 //ACA EL PIN NO VA EL PIN (EJM  2 DEL D2), VA EL GPIO REAL DEL PUERTO DIGITAL.
+#define DHTTYPE DHT21
+DHT dht(DHTPIN, DHTTYPE);
 
 #include <Wire.h> //I2C library
 #include <RtcDS3231.h> //RTC library
 RtcDS3231<TwoWire> rtcObject(Wire);
+
+
 void setup() {
      Serial.begin(115200);  //Starts serial connection
      rtcObject.Begin();     //Starts I2C
-     RtcDateTime currentTime = RtcDateTime(__DATE__, __TIME__);
+     //RtcDateTime currentTime = RtcDateTime(__DATE__, __TIME__);
      rtcObject.SetDateTime(currentTime);
-
+   
+     dht.begin();
+     
 }
 
 void loop() {
+  
  RtcDateTime currentTime = rtcObject.GetDateTime();
 
- int hora = currentTime.Hour();
- int minutos = currentTime.Minute();
+  int hora = currentTime.Hour();
+  int minutos = currentTime.Minute();
 
   Serial.print(hora);
   Serial.print(":");
   Serial.println(minutos);
+
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" °C ");
+
+  
 }
